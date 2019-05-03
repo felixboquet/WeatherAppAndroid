604a2a6816b18d544f830d72e019d26614daec70
Felix Boquet <boquet@brocelia.fr>
2019-05-03 16:43:40 +0200

    Fix Weather realm object

--- app/src/main/java/com/example/fboq/weatherappandroid/Model/Weather.kt
+++ app/src/main/java/com/example/fboq/weatherappandroid/Model/Weather.kt
@@ -1,13 +1,14 @@
 package com.example.fboq.weatherappandroid.Model
 
+import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
 open class Weather(
 
     @PrimaryKey var id: Long = 0,
     var adress: String = "",
-    var date: String?,
+    var date: String? = null,
     var image: String = "",
     var temperature: Double = 0.0
 
-) {}
+) : RealmObject() {}

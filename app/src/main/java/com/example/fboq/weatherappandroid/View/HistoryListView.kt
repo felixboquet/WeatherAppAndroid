bce02047b50f59c79f1c35f9adebbdbdb0e793f4
Felix Boquet <boquet@brocelia.fr>
2019-06-24 16:44:32 +0200

    Fix realm configuration

--- app/src/main/java/com/example/fboq/weatherappandroid/View/HistoryListView.kt
+++ app/src/main/java/com/example/fboq/weatherappandroid/View/HistoryListView.kt
@@ -7,17 +7,21 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.fboq.weatherappandroid.R
 import com.example.fboq.weatherappandroid.services.application.HistoryApplicationLogic
 import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class HistoryListView: AppCompatActivity() {
 
     private lateinit var historyListView: ListView
-    private val realm = Realm.getDefaultInstance()
+    private lateinit var realm: Realm
     private val applicationLogic = HistoryApplicationLogic()
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+        Realm.init(applicationContext)
+        val config = RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build()
+        realm = Realm.getInstance(config)
 
         historyListView = findViewById(R.id.history_list_view)
 
